ce1081f85f6f5b9c902ec8ef1f0ae39bf9b7497a
OleksiiHromovych <gromovuc@gmail.com>
2020-09-17 14:15:44 +0300

    Chapter 3. Vars, vals and types.

---
+++ Sandbox/src/TypeIntro.kt
@@ -0,0 +1,18 @@
+const val MAX_EXPERIENCE = 100
+
+fun main(args: Array<String>) {
+    val playerName = "Estragon"
+    var experiencePoints = 5
+    var hasSteed = false
+    var coins = 50
+    val cafeName = "Unicorn horn"
+    val barmenName = "Barmen"
+    val menu = listOf("root beer", "мclub soda", "coke")
+
+    experiencePoints += 5
+
+    println(experiencePoints)
+    println(playerName)
+    println(playerName.reversed())
+
+}

da85e3edfd7b5df0e1f40715620157b90d8bba68
OleksiiHromovych <gromovuc@gmail.com>
2020-10-02 11:46:32 +0300

    Chapter 8. Numbers

--- NyetHack/src/Tavern.kt
+++ NyetHack/src/Tavern.kt
@@ -1,8 +1,36 @@
+import kotlin.math.roundToInt
+
 const val TAVERN_NAME = "Taernyl's Folly"
+
+var playerGold = 10
+var playerSilver = 10
 
 fun main() {
     placeOrder("shandy, Dragon's Breath, 5.91")
-//    placeOrder("elixir,Shirley's Temple,4.12")
+}
+
+fun performPurchase(price: Double){
+    displayBalance()
+    val totalPurse = playerGold + (playerSilver / 100.0)
+    println("Total purse: $totalPurse")
+    println("Purchase item for $price")
+
+    val remainingBalance = totalPurse - price
+    if (remainingBalance < 0){
+        println("Not enough money to buy item")
+        return
+    }
+    println("Remaining balance: ${"%.2f".format(remainingBalance)}")
+
+    val remainingGold = remainingBalance.toInt()
+    val remainingSilver = (remainingBalance % 1 * 100).roundToInt()
+    playerGold = remainingGold
+    playerSilver = remainingSilver
+    displayBalance()
+}
+
+fun displayBalance() {
+    println("Player's purse balance: Gold: $playerGold, Silver: $playerSilver")
 }
 
 private fun toDragonSpeak(phrase: String) =
@@ -25,6 +53,8 @@ private fun placeOrder(menuData: String) {
     val (type, name, price) = menuData.replace(", ", ",").split(",")
     val message = "Madrigal buys a $name ($type) for $price."
     println(message)
+
+    performPurchase(price.toDouble() ?: 0.0)
 
     val phrase = if (name == "Dragon's Breath") {
         "Madrigal exclaims: ${toDragonSpeak("Ah, delicious $name!")}"
